725496bcd505f96ef02a61b9bbd429bd0917a93e
NidhiGiridhar <nidhigiridhar@gmail.com>
2021-02-11 18:55:30 -0600

    added initial files

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,202 @@
 // product-item.js
 
+// template for product-item element
+const template = document.createElement('template');
+    template.innerHTML = `
+    
+      <style>
+      .price {
+        color: green;
+        font-size: 1.8em;
+        font-weight: bold;
+        margin: 0;
+      }
+      
+      .product {
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        display: grid;
+        grid-template-areas: 
+        'image'
+        'title'
+        'price'
+        'add';
+        grid-template-rows: 67% 11% 11% 11%;
+        height: 450px;
+        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+        margin: 0 30px 30px 0;
+        padding: 10px 20px;
+        width: 200px;
+      }
+      
+      .product > button {
+        background-color: rgb(255, 208, 0);
+        border: none;
+        border-radius: 5px;
+        color: black;
+        justify-self: center;
+        max-height: 35px;
+        padding: 8px 20px;
+        transition: 0.1s ease all;
+      }
+      
+      .product > button:hover {
+        background-color: rgb(255, 166, 0);
+        cursor: pointer;
+        transition: 0.1s ease all;
+      }
+      
+      .product > img {
+        align-self: center;
+        justify-self: center;
+        width: 100%;
+      }
+      
+      .title {
+        font-size: 1.1em;
+        margin: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      
+      .title:hover {
+        font-size: 1.1em;
+        margin: 0;
+        white-space: wrap;
+        overflow: auto;
+        text-overflow: unset;
+      </style>
+
+      <li class="product">
+        <img src="" alt="" width=0>
+        <p class="title"></p>
+        <p class="price"></p>
+        <button onclick="alert('Added to Cart!')">Add to Cart</button>
+      </li> 
+    `;
+
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor() {
+    super();
+
+/*
+    //const s = document.getElementById('product-list');
+    //const shadowRoot = s.attachShadow({mode: 'open'});
+    //const shadowRoot = document.body.main.ul.attachShadow(item);
+    this.attachShadow({mode: 'open'});
+    let item = document.createElement('li'); // Could also use appendChild().
+    item.setAttribute('class', 'product');
+    
+    
+   
+   // title of item
+    let title = document.createElement('p');
+    title.setAttribute('class', 'title');
+    //title.textContent  = this.getAtt
+    //title.setAttribute('value', 'Here inside p');
+    //title.innerHTML('Here inside p');
+
+    // price of item
+    let price = document.createElement('p');
+    price.setAttribute('class', 'price');
+
+    // image of item
+    let image = document.createElement('img');
+    image.setAttribute('src', ''); // fill these in?
+    image.setAttribute('alt', '');
+    image.setAttribute('width', 0);
+
+    // button
+    let button = document.createElement('button');
+    button.setAttribute('onclick', 'alert(Added to Cart!)');
+    button.onclick = () => onclick.click();
+
+    let style = document.createElement('style');
+    style.textContent = `
+    .price {
+      color: green;
+      font-size: 1.8em;
+      font-weight: bold;
+      margin: 0;
+    }
+    
+    .product {
+      align-items: center;
+      background-color: white;
+      border-radius: 5px;
+      display: grid;
+      grid-template-areas: 
+      'image'
+      'title'
+      'price'
+      'add';
+      grid-template-rows: 67% 11% 11% 11%;
+      height: 450px;
+      filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+      margin: 0 30px 30px 0;
+      padding: 10px 20px;
+      width: 200px;
+    }
+    
+    .product > button {
+      background-color: rgb(255, 208, 0);
+      border: none;
+      border-radius: 5px;
+      color: black;
+      justify-self: center;
+      max-height: 35px;
+      padding: 8px 20px;
+      transition: 0.1s ease all;
+    }
+    
+    .product > button:hover {
+      background-color: rgb(255, 166, 0);
+      cursor: pointer;
+      transition: 0.1s ease all;
+    }
+    
+    .product > img {
+      align-self: center;
+      justify-self: center;
+      width: 100%;
+    }
+    
+    .title {
+      font-size: 1.1em;
+      margin: 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    
+    .title:hover {
+      font-size: 1.1em;
+      margin: 0;
+      white-space: wrap;
+      overflow: auto;
+      text-overflow: unset;
+    } `
+    
+
+    this.shadowRoot.appendChild(item);
+    this.shadowRoot.appendChild(style);
+    item.appendChild(image);
+    item.appendChild(title);
+    item.appendChild(price);
+    item.appendChild(button);
+    // document.body.main.ul.appendChild(item);
+    */
+
+
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(template.content.cloneNode(true)); // add template
+    
+    this.shadowRoot.querySelector('.title').innerText = this.getAttribute('title'); // add title
+    console.log(this.getAttribute('title'));
+    
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,32 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
-});
+
+  // create a fetch request to get json data from the url
+  fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => window.localStorage.setItem('products', JSON.stringify(data))); // add fetched data to local storage
+    
+
+  let productArray = JSON.parse(window.localStorage.getItem('products'));
+  for (let i = 0; i < productArray.length; i++) {
+    // create a product item and append it to the #product-list ul element
+    let currentProduct = productArray[i]; // get current product from the json array
+
+
+    let item = document.createElement('product-item');
+    document.getElementById('product-list').appendChild(item);
+
+    // TODO: so this does not work because when line 19 is called, it automatically goes to the ProductItem class and runs the code there 
+    // when that happens, the value of title is null so nothing happens and idk how to fix this!!
+    let productTitle = currentProduct.title
+    item.setAttribute('title', productTitle);
+    console.log(item.getAttribute('title'));
+    
+
+  
+
+    //document.getElementById('product-list').getElementsByClassName('title').innerHTML = currentProduct.title;
+    //console.log(document.getElementById('product-list').getElementsByClassName('title'));
+  }
+})
